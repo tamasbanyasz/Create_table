42268328c04ea83895d4ea4dfa72857687e9c0f2
Bányász Tamás <116582308+tamasbanyasz@users.noreply.github.com>
2023-04-26 15:01:50 +0200

    FlaskApp

--- storedatas/models.py
+++ storedatas/models.py
@@ -5,5 +5,5 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String)
     lastname = db.Column(db.String)
-    date = db.Column(db.DateTime)
+    date = db.Column(db.String)
     
