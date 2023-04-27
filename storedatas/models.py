c511a68e812bbdff62741d1189cc7e504d8c5e70
Bányász Tamás <116582308+tamasbanyasz@users.noreply.github.com>
2023-04-27 14:41:43 +0200

    Delete storedatas directory

--- storedatas/models.py
+++
@@ -1,9 +0,0 @@
-from storedatas import db
-
-
-class Employee(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String)
-    lastname = db.Column(db.String)
-    date = db.Column(db.String)
-    
